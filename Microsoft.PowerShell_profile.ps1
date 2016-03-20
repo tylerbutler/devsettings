6f2eab63a0b13218749b23d688835d31fdb2d226
Tyler Butler <tyler@tylerbutler.com>
2016-03-20 14:22:28 -0700

    PowerShell profile updates

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,11 @@
 ﻿### POSH GIT START ###
 Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
-# If module is installed in a default location ($env:PSModulePath),
-# use this instead (see about_Modules for more information):
 Import-Module posh-git
 
 # Set up a simple prompt, adding the git prompt parts inside git repos
 function global:prompt {
     $realLASTEXITCODE = $LASTEXITCODE
-
-    # Reset color, which can be messed up by Enable-GitColors
-    $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
 
     Write-Host($pwd.ProviderPath) -nonewline
 
@@ -19,8 +14,6 @@ function global:prompt {
     $global:LASTEXITCODE = $realLASTEXITCODE
     return "> "
 }
-
-Enable-GitColors
 
 Pop-Location
 
