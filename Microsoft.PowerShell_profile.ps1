5a2b83ca8fe4b741b336774b0bde91b71b065b0c
Tyler Butler <tyler@tylerbutler.com>
2019-10-03 18:57:12 -0700

    Update PowerShell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,25 +1,14 @@
-﻿### POSH GIT START ###
-Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
-
-Import-Module posh-git
-
-# Set up a simple prompt, adding the git prompt parts inside git repos
-function global:prompt {
-    $realLASTEXITCODE = $LASTEXITCODE
-
-    Write-Host($pwd.ProviderPath) -nonewline
-
-    Write-VcsStatus
-
-    $global:LASTEXITCODE = $realLASTEXITCODE
-    return "> "
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
 }
 
-Pop-Location
-
-Start-SshAgent -Quiet
-
-### POSH GIT END ###
+# Prompt
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Agnoster
+Set-Variable -name DefaultUser 'tylerbu'
 
 # Aliases and their functions
 function subl { &"${Env:ProgramFiles}\Sublime Text 2\sublime_text.exe" $args }
@@ -29,6 +18,3 @@ function windows_explorer { explorer . }
 New-Alias e windows_explorer
 New-Alias ex explorer
 New-Alias st subl
-
-# Load Jump-Location profile
-Import-Module 'C:\ProgramData\chocolatey\lib\Jump-Location.0.6.0\tools\Jump.Location.psd1'
